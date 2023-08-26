5d1911fac03dd2043afa71f710aeeee52dd94deb
BlueKitsune狐 <saadshaan619@gmail.com>
2023-08-26 19:28:07 +0500

    Ccommitting~~

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print(hello world);
+def tell_joke():
+    print("Why don't scientists trust atoms?")
+    input("Press Enter for the punchline...")
+
+def punchline():
+    print("Because they make up everything!")
+
+def main():
+    tell_joke()
+    punchline()
+
+if __name__ == "__main__":
+    main()
